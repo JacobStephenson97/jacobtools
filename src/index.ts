300dbf0d151a9781ca659afd1655e2de271b79d2
Jacob Stephenson <jacobstephenson97@gmail.com>
2023-02-13 14:16:52 -0500

    missed word

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
 #!/usr/bin/env node
 
-import inquirer from "inquirer";
-import path from "path";
-import fs from "fs-extra";
-import chalk from "chalk";
-import { baseTemplateDirectory, templateDirectory } from "./consts.js";
-import getPackageIndex from "./utils/getPackageIndex.js";
-import { updateIndex } from "./helpers/updateIndex.js";
-import { getPackageJson } from "./helpers/packages.js";
-import { requiredFastifyPackages } from "./packages/fastify/index.js";
-import { getUserPackageManager } from "./utils/getPackageManager.js";
-import { installDeps } from "./helpers/installDeps.js";
 import { startApp } from "./app/startApp.js";
 
 export type AppOptions = {
